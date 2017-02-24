7eaad32edaba0aef6824f7ea638f2925dce6374f
Lucie Becz <luciebecz@yahoo.com>
2017-02-24 16:53:48 -0700

    second commit, no edit capability

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -10,19 +10,34 @@ $(document).ready( () => {
       dataType: 'JSON'
     }).done( data => {
       data.forEach( user => {
-        $users.append(`
+        $('#users').append(`
                         <li>
                           ${user.first_name}
                           <button data-id=${user.id} class='view-btn'>View User</button>
+                          <button data-id=${user.id} class='delete-btn'>Delete User</button>
+                          <button data-id=${user.id} class='edit-btn'>Edit User</button>
                         </li>
-                      `)
+                      `);
       });
-    }).fail( data => {
-        console.log(data)
+    }).fail( error => {
+        console.log(error)
   });
  
 }
 fetchUsers();
+
+$(document).on('click', '.delete-btn', (e) => {
+  let userId = $(e.target).data('id');
+  $.ajax({
+    url: `${baseUrl}/users/${userId}`,
+    type: 'DELETE',
+    dataType: 'JSON'
+  }).done( data => {
+    fetchUsers();
+  }).fail( data => {
+    console.log(data);
+  });
+});
 
 $(document).on('click', '.view-btn', (e) => {
   let userId = $(e.target).data('id');
@@ -31,15 +46,31 @@ $(document).on('click', '.view-btn', (e) => {
     type: 'GET',
     dataType: 'JSON' 
   }).done( user => {
-    let $userInfo = $('user-info')
+    let $userInfo = $('#user-info');
     $userInfo.html(`
       <p>${user.first_name}</p>
       <p>${user.last_name}</p>
       <p>${user.phone_number}</p>
     `);
     $userInfo.show();
-  }).fail( user => {
-    console.log(data);
+  }).fail( error => {
+    console.log(error);
   });
 });
-})
+let $userForm = $('#user-form')
+$userForm.submit( (e) => {
+  e.preventDefault();
+  $.ajax({
+    url: `${baseUrl}/users`,
+    type: 'POST',
+    dataType: 'JSON',
+    data: $userForm.serializeArray()
+  }).done( data => {
+    $userForm[0].reset();
+    $('#user-first-name').focus();
+    fetchUsers();
+  }).fail(error => {
+    console.log(error);
+  });
+});
+});
